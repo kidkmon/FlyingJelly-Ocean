63391e644e5265fedbf645b5b43c5ce6a09d0e78
ocean <ocean@oceanmanaus.com>
2018-06-19 13:13:53 -0400

    first commit

---
+++ Assets/Scripts/Ground/MoveOffset.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOffset : MonoBehaviour {
+
+
+	private Material currentMaterial;
+	private float offset;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		currentMaterial = GetComponent<SpriteRenderer>().material;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += 0.001f;
+
+		currentMaterial.SetTextureOffset("_MainTex", new Vector2(offset * speed, 0));
+	}
+}
